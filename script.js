cfcfd305fc5aa638772195fff662cea3abaf8fe0
ChrisStyles99 <chrisrdz99@hotmail.com>
2020-08-24 23:47:35 -0500

    changed card height

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const animeCards = document.querySelector('.anime-cards');
 
 btnSearch.addEventListener('click', async() => {
   try {
+    animeCards.innerHTML = '';
     const res = await fetch(`https://api.jikan.moe/v3/search/anime?q=${input.value}`);
     const data = await res.json();
     console.log(data);
@@ -15,7 +16,7 @@ btnSearch.addEventListener('click', async() => {
         </div>
         <div class="card-content">
           <h3>${data.results[i].title}</h3>
-          <h4>Score: ${data.results[i].score}</h4>
+          <h4>Score: ${data.results[i].score}/10</h4>
         </div>
       </div>
       `
